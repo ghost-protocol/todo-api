10a09b5bd6056294c2c5c4acdcaa402fd8a9f4d9
Kwame Davour <kwamedavour@gmail.com>
2016-09-04 09:50:16 +0000

    api

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson(obj){
+// 	// obj={
+// 	// 	name:'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//array Example
+var grades = [125, 88];
+
+function addGrades(gradesArr){
+	//gradesArr.push(99);
+	gradesArr = [22,33,44];
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
