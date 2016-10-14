4f21db61583f90f525b7896905623ed063591665
Kwame Davour <kwamedavour@gmail.com>
2016-10-14 15:49:19 +0000

    Add auth token generation to POS /users/login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -171,11 +172,30 @@ app.post('/users', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function (user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
 });
+
+//POST /users/login
+app.post('/users/login', function(req, res) {
+	var body =_.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user) {
+		var token = user.generateToken('authentication');
+
+		if (token){
+			res.header('Auth', token).json(user.toPublicJSON);
+		} else{
+			res.status(401).send();
+		}
+		//res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
+	});
+});
+
 	//var validAttributes ={};
 
 	// if (!matchedTodo){
@@ -198,7 +218,8 @@ app.post('/users', function(req, res){
 	// res.json(matchedTodo);
 	//});
 
-db.sequelize.sync().then(function (){
+//sync and add promise callback
+db.sequelize.sync({force: true}).then(function (){
 	app.listen(PORT, function () {
 		console.log('Express listening on port ' + PORT + '!');
 	});
